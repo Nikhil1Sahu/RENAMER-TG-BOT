b2fc6ff298a8055412c7bb508a985a0e149cfceb
Nikhil1Sahu <168706708+Nikhil1Sahu@users.noreply.github.com>
2025-08-12 00:42:23 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -2,29 +2,26 @@ import os
 import logging
 logger = logging.getLogger(__name__)
 
-
 class Config:
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
-    AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS", "") else []
-    if OWNER_ID not in AUTH_USERS:
-        AUTH_USERS.append(OWNER_ID)
-    BANNED_USERS = [int(i) for i in os.environ.get("BANNED_USERS", "").split(" ")] if os.environ.get("BANNED_USERS", "") else None
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
-    CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    API_ID = 18466881
+    API_HASH = "8c8ca14ad8e416ce4e6ea717db7ec6af"
+    OWNER_ID = 5565120414
+    AUTH_USERS = [8190474898, 1918675169, OWNER_ID]
+    BANNED_USERS = []
+    BOT_TOKEN = "8483246378:AAFACrQv9a9rSfLjejyIAClK2b8iokIeARo"
+    BOT_PASSWORD = None
+    CUSTOM_CAPTION = None
+    FORCE_SUB = "network_of_kingdom"
+    DATABASE_URL = "mongodb+srv://nikhilsahu7j:dTQKfvo0jABOYKOu@cluster0.n2csgvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     try:
-        TIME_GAP = int(os.environ.get("TIME_GAP", "")) if os.environ.get("TIME_GAP", "") else None
+        TIME_GAP = None
     except:
         TIME_GAP = None
         logger.warning("Give the timegap in seconds. Dont use letters 😑")
     TIME_GAP_STORE = {}
     try:
-        TRACE_CHANNEL = int(os.environ.get("TRACE_CHANNEL")) if os.environ.get("TRACE_CHANNEL", "") else None
+        TRACE_CHANNEL = -1002731391701
     except:
         TRACE_CHANNEL = None
         logger.warning("Trace channel id was invalid")
